1d894e917f38e43103f21356a93e08b4c0e1aaee
yuki-zirushi <yuko.12.151@gmail.com>
2021-09-01 09:40:09 +0000

    [Add] error message

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -14,9 +14,11 @@ class BlogsController < ApplicationController
 
   def create
     blog = Blog.new(blog_params)
-    blog.save
-    redirect_to blogs_path
-    redirect_to blog_path(blog.id)
+    if blog.save
+     redirect_to blog_path(blog.id)
+    else
+     render :new
+    end
   end
 
   def edit
